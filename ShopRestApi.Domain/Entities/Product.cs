634bc945b0c4e8cccf9a4965f3f047ee268cc295
msrtna <mohalimor64@gmail.com>
2026-06-13 19:18:15 +0330

    add product entity

---
+++ ShopRestApi.Domain/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopRestApi.Domain.Entities
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
